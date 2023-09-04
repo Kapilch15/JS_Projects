e4bdc786faea65dea33b0fe381ecbabfa2ae04fa
Kapil15 <Chaudhary99Kapil@gmail.com>
2023-09-04 11:00:42 +0530

    Search bar update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function DisplayInfo() {
     statement += `<tr>
     <th scope="row">${index + 1}</th>
     <td>${user.name}</td>
-    <td><i class="btn text-white btn-info fa-solid fa-pen-to-square" onclick='EditInfo(${index})'></i><i class="btn text-white btn-danger fa-solid fa-trash mx-2" onclick='DeleteInfo(${index})'></i></td>
+    <td><i class="btn text-white btn-info fa-solid fa-pen-to-square" onclick='EditInfo(${index})'></i><i class="btn text-white btn-danger fa-regular fa-minus mx-3" onclick='DeleteInfo(${index})'></i></td>
     </tr>`;
   });
   recordsDisplay.innerHTML = statement;
@@ -57,3 +57,22 @@ function DeleteInfo(id) {
   userArray.splice(id, 1);
   saveInfo(userArray);
 }
+
+//search feathures
+const allTr = document.querySelectorAll("#records tr");
+const SearchInput = document.getElementById("search");
+
+SearchInput.addEventListener("input", function (e) {
+  // console.log(e.target.value);
+  const searchStr = e.target.value.toLowerCase();
+  recordsDisplay.innerHTML = "";
+  allTr.forEach((tr) => {
+    const td_in_tr = tr.querySelectorAll("td");
+    if (td_in_tr[0].innerText.toLowerCase().indexOf(searchStr) > -1) {
+      recordsDisplay.appendChild(tr);
+    }
+  });
+  if (recordsDisplay.innerHTML == "") {
+    recordsDisplay.innerHTML = "Record not Found";
+  }
+});
